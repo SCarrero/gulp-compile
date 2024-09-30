5f16e4076e849f2d41f4f7b286a3473be02cbd49
S Carrero <sherry_carrero@freddiemac.com>
2024-09-30 22:52:54 +0000

    SassJson: Added browserlist support.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const sass = require('gulp-sass')(require('sass-embedded'));
 const autoprefixer = require('autoprefixer');
 const postcss = require('gulp-postcss');
 const sourcemaps = require('gulp-sourcemaps');
-const cleancss = require('gulp-cleaner-css');
 const mode = require('gulp-mode')();
 
 function compileCss(done) {
@@ -13,17 +12,16 @@ function compileCss(done) {
   // Define a task to compile main SCSS
   gulp.src('scss/style.scss')
     .pipe((mode.development(sourcemaps.init())))
-    .pipe(sass({api: "modern"}).on('error', sass.logError)) 
+    .pipe(sass({api: "modern", outputStyle: "compressed"}).on('error', sass.logError)) 
     .pipe(postcss(postCssPlugins))
-    .pipe((mode.production(cleancss())))
     .pipe((mode.development(sourcemaps.write())))
     .pipe(gulp.dest('css'));
   // Define a task to compile Custom standaone SCSS
   gulp.src('scss/custom/*.scss')
     .pipe((mode.development(sourcemaps.init())))
-    .pipe(sass({api: "modern"}).on('error', sass.logError))
+    .pipe((mode.development(sass({api: "modern"}).on('error', sass.logError))))
+    .pipe((mode.production(sass({api: "modern", outputStyle: "compressed"}).on('error', sass.logError))))
     .pipe(postcss(postCssPlugins))
-    .pipe((mode.production(cleancss())))
     .pipe((mode.development(sourcemaps.write())))
     .pipe(gulp.dest('css/custom'));
   done();
