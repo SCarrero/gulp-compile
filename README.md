d4dcc46b68a81a30100f2cbc07ec3ec7e5ebf702
SCarrero <sherry_carrero@freddiemac.com>
2024-09-30 10:01:20 -0400

    Master: Initial Code creation.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# gulp-compile
-Test to revamp our workflow to speed up css compiling
+# Sass Compile with Gulp
+
+Run 'npm run watch' or 'yarn watch' to do a development compile with sourcemaps and set a scss watcher.
+
+Run 'npm run build' or 'yarn build' to do a production compile with minification.
+
+To Do: Add in js compiling.
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass-embedded'));
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const cleancss = require('gulp-cleaner-css');
+const mode = require('gulp-mode')();
+
+function compileCss(done) {
+  const postCssPlugins = [
+    autoprefixer()
+  ].filter(Boolean);
+  // Define a task to compile main SCSS
+  gulp.src('scss/style.scss')
+    .pipe((mode.development(sourcemaps.init())))
+    .pipe(sass().on('error', sass.logError)) 
+    .pipe(postcss(postCssPlugins))
+    .pipe((mode.production(cleancss())))
+    .pipe((mode.development(sourcemaps.write())))
+    .pipe(gulp.dest('css'));
+  // Define a task to compile Custom standaone SCSS
+  gulp.src('scss/custom/*.scss')
+    .pipe((mode.development(sourcemaps.init())))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(postCssPlugins))
+    .pipe((mode.production(cleancss())))
+    .pipe((mode.development(sourcemaps.write())))
+    .pipe(gulp.dest('css/custom'));
+  done();
+} 
+
+gulp.task('watcher', function() {
+  gulp.watch('scss/**/*.scss', compileCss);  
+});
+
+gulp.task('watch', gulp.series(compileCss, 'watcher'));
+gulp.task('build', compileCss);
