166cf9c37efefc6eedae17fd866e0b31f3976af4
S Carrero <sherry_carrero@freddiemac.com>
2024-10-02 15:35:13 +0000

    Clean: Add clean function to delete css prior to compile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass-embedded'));
-const autoprefixer = require('autoprefixer');
-const postcss = require('gulp-postcss');
-const sourcemaps = require('gulp-sourcemaps');
-const mode = require('gulp-mode')();
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass-embedded'));
+var autoprefixer = require('autoprefixer');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var mode = require('gulp-mode')(["production", "development"]);
+var cleanSweep = require("clean-sweep");
+var cleanDest = 'css';
 
+function clean(done) {
+  try {
+    cleanSweep.sweepSync(cleanDest, {
+      verbose: true,
+      threshHold: 9999999
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+  done();
+}
 function compileCss(done) {
   const postCssPlugins = [
     autoprefixer()
@@ -12,17 +25,18 @@ function compileCss(done) {
   // Define a task to compile main SCSS
   gulp.src('scss/style.scss')
     .pipe((mode.development(sourcemaps.init())))
-    .pipe(sass({api: "modern", outputStyle: "compressed"}).on('error', sass.logError)) 
+    .pipe(mode.development(sass({api: "modern-compiler"}).on('error', sass.logError))) 
+    .pipe(mode.production(sass({outputStyle: "compressed"}).on('error', sass.logError))) 
     .pipe(postcss(postCssPlugins))
     .pipe((mode.development(sourcemaps.write())))
     .pipe(gulp.dest('css'));
   // Define a task to compile Custom standaone SCSS
   gulp.src('scss/custom/*.scss')
-    .pipe((mode.development(sourcemaps.init())))
-    .pipe((mode.development(sass({api: "modern"}).on('error', sass.logError))))
-    .pipe((mode.production(sass({api: "modern", outputStyle: "compressed"}).on('error', sass.logError))))
+    .pipe(mode.development(sourcemaps.init()))
+    .pipe(mode.development(sass().on('error', sass.logError)))
+    .pipe(mode.production(sass({outputStyle: "compressed"}).on('error', sass.logError)))
     .pipe(postcss(postCssPlugins))
-    .pipe((mode.development(sourcemaps.write())))
+    .pipe(mode.development(sourcemaps.write()))
     .pipe(gulp.dest('css/custom'));
   done();
 } 
@@ -31,5 +45,5 @@ gulp.task('watcher', function() {
   gulp.watch('scss/**/*.scss', compileCss);  
 });
 
-gulp.task('watch', gulp.series(compileCss, 'watcher'));
-gulp.task('build', compileCss);
+gulp.task('watch', gulp.series(clean, compileCss, 'watcher'));
+gulp.task('build', gulp.series(clean, compileCss));
